33ec850e263519eb99237fe5c64de31118caa17c
Chris Alberti <christopherjalberti@gmail.com>
2017-12-09 23:12:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destrutivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
