1de74d706df9996b96658b287dfc0fe190bfbc43
Chris Alberti <christopherjalberti@gmail.com>
2017-12-09 23:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = "I don't do much.";
 
-function destructivelyAppendKitten(name) {
-  var kittens = [];
-  kittens.push(name);
-  return kittens;
+function destructivelyAppendKitten(array, name) {
+  return array.push(name);
 }
